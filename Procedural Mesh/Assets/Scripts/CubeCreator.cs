df7278e9d43f930870db2e022310255afb55845a
aidankennell <32394698+aidankennell@users.noreply.github.com>
2018-04-24 12:10:39 -0700

    done

---
+++ Procedural Mesh/Assets/Scripts/CubeCreator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+
+public class CubeCreator : MonoBehaviour
+{
+    public Vector3 size = Vector3.one;
+    public Vector2 pos = new Vector2(0,0);
+    private float timer = 0;
+    private float timeMod = .5f;
+    private float modifier = 1f;
+    private float posMod = 10f;
+
+
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime * timeMod;
+
+        if(timer > 255)
+        {
+            timer = 0;
+        }
+        
+
+        MeshFilter meshFilter = this.GetComponent<MeshFilter>();    // one submesh for each face 
+        MeshCreator mc = new MeshCreator();
+
+        Vector3 cubeSize = size * 0.5f;
+
+        float noiseValueT0 = Perlin.Noise((pos.x + cubeSize.x) / posMod, (pos.y - cubeSize.z) / posMod, timer);
+        float noiseValueT1 = Perlin.Noise((pos.x - cubeSize.x) / posMod, (pos.y - cubeSize.z) / posMod, timer);
+        float noiseValueT2 = Perlin.Noise((pos.x - cubeSize.x) / posMod, (pos.y + cubeSize.z) / posMod, timer);
+        float noiseValueT3 = Perlin.Noise((pos.x + cubeSize.x) / posMod, (pos.y + cubeSize.z) / posMod, timer);
+        //float noiseValueT0 = Perlin.Noise((pos.x + cubeSize.x) * posMod, (pos.y - cubeSize.z) * posMod, timer);
+        //float noiseValueT1 = Perlin.Noise((pos.x - cubeSize.x) * posMod, (pos.y - cubeSize.z) * posMod, timer);
+        //float noiseValueT2 = Perlin.Noise((pos.x - cubeSize.x) * posMod, (pos.y + cubeSize.z) * posMod, timer);
+        //float noiseValueT3 = Perlin.Noise((pos.x + cubeSize.x) * posMod, (pos.y + cubeSize.z) * posMod, timer);
+
+        // top of the cube        
+        // t0 is top left point        
+        Vector3 t0 = new Vector3(cubeSize.x, cubeSize.y + modifier * noiseValueT0, -cubeSize.z);
+        Vector3 t1 = new Vector3(-cubeSize.x, cubeSize.y + modifier * noiseValueT1, -cubeSize.z);
+        Vector3 t2 = new Vector3(-cubeSize.x, cubeSize.y + modifier * noiseValueT2, cubeSize.z);
+        Vector3 t3 = new Vector3(cubeSize.x, cubeSize.y + modifier * noiseValueT3, cubeSize.z);
+
+        // bottom of the cube       
+        Vector3 b0 = new Vector3(cubeSize.x, -cubeSize.y, -cubeSize.z);
+        Vector3 b1 = new Vector3(-cubeSize.x, -cubeSize.y, -cubeSize.z);
+        Vector3 b2 = new Vector3(-cubeSize.x, -cubeSize.y, cubeSize.z);
+        Vector3 b3 = new Vector3(cubeSize.x, -cubeSize.y, cubeSize.z);
+
+        // Top square        
+        mc.BuildTriangle(t0, t1, t2);
+        mc.BuildTriangle(t0, t2, t3);
+
+        // Bottom square        
+        mc.BuildTriangle(b2, b1, b0);
+        mc.BuildTriangle(b3, b2, b0);
+
+        // Back square        
+        mc.BuildTriangle(b0, t1, t0);
+        mc.BuildTriangle(b0, b1, t1);
+
+        mc.BuildTriangle(b1, t2, t1);
+        mc.BuildTriangle(b1, b2, t2);
+
+        mc.BuildTriangle(b2, t3, t2);
+        mc.BuildTriangle(b2, b3, t3);
+
+        mc.BuildTriangle(b3, t0, t3);
+        mc.BuildTriangle(b3, b0, t0);
+
+        meshFilter.mesh = mc.CreateMesh();
+    }
+}
---
+++ Procedural Mesh/Assets/Scripts/FieldManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldManager : MonoBehaviour {
+
+    //private List<Transform> cubes = new List<Transform>();
+    private Transform[,] cubes = new Transform[20, 20];
+    public Transform cube;
+    private int sampleSize = 10;
+    private int region = 0;
+    private float distanceRatio = 0f;
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < 20; x++)
+        {
+            for(int z = 0; z < 20; z++)
+            {
+                cubes[x,z] = Instantiate(cube, new Vector3(((float)x + (float)x * distanceRatio), 0, (float)z + (float)z * distanceRatio), Quaternion.identity);
+                cubes[x, z].gameObject.GetComponent<CubeCreator>().pos = new Vector2((float)x, (float)z);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
